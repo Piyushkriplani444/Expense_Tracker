f83b4d90199097b076658b9d107150718ac65276
Piyush Kriplani <piyushkriplani444@gmail.com>
2022-11-17 22:37:26 +0530

    Added expeses form and show data with delete option

--- app/src/Expenses.js
+++ app/src/Expenses.js
@@ -3,7 +3,6 @@ import AppNav from './AppNav';
 import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
 import { Table,Container,Input,Button,Label, FormGroup, Form} from 'reactstrap';
 import {Link} from 'react-router-dom';
 import Moment from 'react-moment';
@@ -16,26 +15,184 @@ class Expenses extends Component {
 // "person":
 // {"id":1,"name":"Codeengine11@gmail.com","email":"Siamak"},
 // "location":"New York"}]
+   emptyItem = {
+      id:104,
+      description: '',
+      expensedate : new Date(),
+      location:'',
+      category:{
+        id: 1,name:'Travel'
+      }
+   }
+
+   constructor(props){
+    super(props)
+
+    this.state = {
+        isLoading : false,
+        Categories : [],
+        Expenses : [],
+        date : new Date(),
+        item : this.emptyItem
+    }
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleChange = this.handleChange.bind(this);
+    this.handleDateChange = this.handleDateChange.bind(this);
+
+   }
+
+   async handleSubmit(event){
+    const item= this.state.item;
+
+    await fetch(`/api/expenses`,{
+        method: 'POST',
+        headers :{
+            'Accept' : 'application/json',
+            'Content-Type' : 'application/json'
+        },
+        body : JSON.stringify(item),
+    });
+     event.preventDefault();
+     this.props.history.push("/expenses");
+   }
+
+   handleChange(event){
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+    let item ={...this.state.item};
+    item[name]= value;
+    this.setState({item});
+      console.log(item);
+    }
+
+    handleDateChange(date){
+        let item ={...this.state.item};
+        item.expensedate = date;
+        this.setState({item});
+    }
+
+    async remove(id){
+        await fetch(`/api/expenses/${id}`,{
+            method : 'DELETE',
+            headers :{
+                'Accept' : 'application/json',
+                'Content-Type' : 'application/json'
+            }
+        }).then(()=>{
+            let updatedExpenses = [...this.state.Expenses].filter(i => i.id !== id);
+            this.setState({Expenses: updatedExpenses});
+        });
+    }
+
+    async componentDidMount (){
+        const response= await fetch('/api/categories');
+        const body= await response.json();
+        this.setState({Categories : body , isLoading :false});
+
+
+        const responseExp= await fetch('/api/expenses');
+        const bodyExp = await responseExp.json();
+        this.setState({Expenses : bodyExp , isLoading :false});
+        console.log(bodyExp);
+    }
+
     state={ }
     render(){
         const title =<h3>Add Expense</h3>;
+        const {Categories} =this.state;
+        const {Expenses,isLoading} = this.state;
+        console.log(Expenses);
+
+        if (isLoading)
+            return(<div>Loading....</div>)
+        
+
+
+        let optionList  =
+                Categories.map( (category) =>
+                    <option value={category.id} key={category.id}>
+                                {category.name} 
+                    </option>
+                )
+        
+        let rows=
+            Expenses.map( expense =>
+              <tr key={expense.id}>
+                <td>{expense.description}</td>
+                <td>{expense.location}</td>
+                <td><Moment date={expense.expensedate} format="YYYY/MM/DD"/></td>
+                <td>{expense.category.name}</td>
+                <td><Button size="sm" color="danger" onClick={() => this.remove(expense.id)}>Delete</Button></td>
+
+              </tr>
+
+
+            )
+        
+
         return (
         <div>
             <AppNav />
             <Container>
                {title}
-                <Form >
-                 <FormGroup>
-                       <Label for="description">Title</Label>
-                       <Input type="description" name="description" id="description" autoComplete="name" /> 
-                 </FormGroup>
-                  
-                
-                
-                
-                </Form>
-            </Container>
+               <Form onSubmit={this.handleSubmit}>
+                    <FormGroup>
+                        <Label for="description">Title</Label>
+                        <Input type="description" name="description" id="description" 
+                            onChange={this.handleChange} autoComplete="name"/>
+                    
+                    </FormGroup>
 
+                    <FormGroup>
+                        <Label for="category" >Category</Label>
+                        <select onChange={this.handleChange}>
+                                {optionList}
+                        </select>
+                    
+                    </FormGroup>
+
+                    <FormGroup>
+                        <Label for="city">Date</Label>
+                        <DatePicker    selected={this.state.item.expensedate}  onChange={this.handleDateChange} />
+                    </FormGroup>
+
+                    <div className="row">
+                        <FormGroup className="col-md-4 mb-3">
+                        <Label for="location">Location</Label>
+                        <Input type="text" name="location" id="location" onChange={this.handleChange}/>
+                        </FormGroup>
+                      
+                    </div>
+                    <FormGroup>
+                        <Button color="primary" type="submit">Save</Button>{' '}
+                        <Button color="secondary" tag={Link} to="/">Cancel</Button>
+                    </FormGroup>
+                    </Form>
+                </Container>
+              
+
+          {''}
+              <Container>
+                <h3>Expense List</h3>
+                <Table className="mt-4">
+                <thead>
+                  <tr>
+                    <th width="30%">Description</th>
+                    <th width="10%">Location</th>
+                    <th> Date</th>
+                    <th> Category</th>
+                    <th width="10%">Action</th>
+                  </tr>
+                </thead>
+                <tbody>
+                   {rows}
+                </tbody>
+
+                </Table>
+              </Container>
+
+          
 
         </div> );
     }
