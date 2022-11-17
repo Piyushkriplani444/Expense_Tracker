e6ff231ff130b361a2c15d8862c8ff6b3141ba38
Piyush Kriplani <piyushkriplani444@gmail.com>
2022-11-17 18:31:37 +0530

    added navbar for navigation

---
+++ app/src/AppNav.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+    Navbar,
+    NavItem,
+    NavLink,
+    Nav,
+    NavbarBrand
+} from 'reactstrap'
+
+
+class AppNav extends Component {
+    state = {  }
+    render() {
+        return (
+          <div>
+            <Navbar color="dark" dark  expand="md">
+              <NavbarBrand href="/">Expense Tracker Application</NavbarBrand>
+                <Nav className="ml-auto" navbar>
+                  <NavItem>
+                    <NavLink href="/">Home</NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink href="/categories">Categories</NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink href="/expenses">Expenses</NavLink>
+                  </NavItem>
+                
+                </Nav>
+          
+            </Navbar>
+          </div>
+        );
+      }
+}
+ 
+export default AppNav;
+
+
